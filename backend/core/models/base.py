1bfe2e08002a614befbe7d0ef926e025b0c3be2d
mist258 <natalia.kolchuk@gmail.com>
2026-02-24 23:52:46 +0200

    added class Base

---
+++ backend/core/models/base.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr
+from utils import camel_case_to_snake_case
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = Column(Integer, primary_key=True)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
