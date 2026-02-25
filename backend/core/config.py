14b5884a0b10c9e7d90a4755ec343c7a3dbc5732
mist258 <natalia.kolchuk@gmail.com>
2026-02-25 08:13:03 +0200

    added naming convention

--- backend/core/config.py
+++ backend/core/config.py
@@ -10,12 +10,21 @@ class RunConfig(BaseModel):
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
+
 class DatabaseConfig(BaseSettings):
     url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 20
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
 
 
 class Settings(BaseSettings):
--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,9 +1,15 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr
+
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
 
     id: Mapped[int] = Column(Integer, primary_key=True)
 
