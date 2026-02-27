910c7ddc8ffb430ffd777edd9388d4e918b1cafe
mist258 <natalia.kolchuk@gmail.com>
2026-02-27 14:50:24 +0200

    connected routers

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
+from .users.routers import users_router
+from .auth.routers import auth_router
+from .articles.routers import article_router
 
 router = APIRouter()
+router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(users_router, prefix="/users", tags=["users"])
+router.include_router(article_router, prefix="/articles", tags=["articles"])
