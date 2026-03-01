c2e60048b2a29fa1648e6a17983baf17ecbb0e83
mist258 <natalia.kolchuk@gmail.com>
2026-03-01 03:43:48 +0200

    deleted

--- backend/api/auth/schemas.py
+++ backend/api/auth/schemas.py
@@ -1,21 +1,4 @@
-from pydantic import BaseModel, field_validator, ConfigDict, EmailStr
-import re
-
-class SetPasswordSchema(BaseModel):
-    model_config = ConfigDict(strict=True)
-     # token to set password you have to get from URL  todo
-    password: str
-
-    @field_validator('password')
-    @classmethod
-    def validate_received_password(cls, password: str) -> str:
-
-        pattern = r"^[A-Za-z\d@$!%*?&]{8,}$"
-
-        if not re.fullmatch(pattern, password):
-            raise ValueError('Password must contain at least 8 characters, '
-                                  '1 special symbol, 1 letter, 1 number')
-        return password
+from pydantic import BaseModel, EmailStr
 
 # for user login
 class UserLoginSchema(BaseModel):
@@ -26,5 +9,7 @@ class UserLoginSchema(BaseModel):
 # schema for token
 class TokenInfoSchema(BaseModel):
     access_token: str
-    token_type: str
+    refresh_token: str
+    token_type: str = "Bearer"
+
 
